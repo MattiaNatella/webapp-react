809230d3a956fe5d214e23f79e35e2c132ff65e4
MattiaNatella <mattianatella@gmail.com>
2025-02-11 13:03:39 +0100

    [add] upload preview + handleChange

--- src/pages/FilmForm.jsx
+++ src/pages/FilmForm.jsx
@@ -1,14 +1,55 @@
+import { useState } from "react"
+import { generatePath } from "react-router-dom"
 
 
 const FilmForm = () => {
+
+    const initialFilmData = {
+        title: '',
+        genre: '',
+        release_year: '',
+        director: '',
+        image: null,
+        abstract: ''
+    }
+
+    const [newFilm, setNewFilm] = useState(initialFilmData)
+    const [imagePreview, setImagePreview] = useState('https://placehold.co/200x150')
+
+    const handleFilmChange = (e) => {
+        const { name, value } = e.target
+        console.log(e.target.value)
+        if (name == 'image') {
+            setImagePreview(URL.createObjectURL(e.target.files[0]));
+            setNewFilm(prev => ({
+                ...prev,
+                image: e.target.files[0]
+            }))
+        } else {
+            setNewFilm(prev => ({
+                ...prev,
+                [name]: value
+            }))
+        }
+        console.log(newFilm)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className="card">
             <h5 className="card-header bg-dark text-white">Aggiungi un film alla lista</h5>
             <div className="card-body">
-                <form action="#">
+                <form action="#" onSubmit={handleSubmit}>
                     <div className="input-group mb-3">
                         <span className="input-group-text">Titolo</span>
                         <input
+                            name="title"
+                            value={newFilm.title}
+                            onChange={handleFilmChange}
                             type="text"
                             className="form-control"
                             aria-label="Sizing example input"
@@ -18,6 +59,9 @@ const FilmForm = () => {
                         <span className="input-group-text">Genere</span>
                         <input
                             type="text"
+                            name="genre"
+                            value={newFilm.genre}
+                            onChange={handleFilmChange}
                             className="form-control"
                             aria-label="Sizing example input"
                             aria-describedby="inputGroup-sizing-default" />
@@ -26,6 +70,9 @@ const FilmForm = () => {
                     <div className="input-group mb-3">
                         <span className="input-group-text" id="inputGroup-sizing-default">Anno di pubblicazione</span>
                         <input
+                            name="release_year"
+                            value={newFilm.release_year}
+                            onChange={handleFilmChange}
                             type="text"
                             className="form-control"
                             aria-label="Sizing example input"
@@ -35,6 +82,9 @@ const FilmForm = () => {
                     <div className="input-group mb-3">
                         <span className="input-group-text" id="inputGroup-sizing-default">Director</span>
                         <input
+                            name="director"
+                            value={newFilm.director}
+                            onChange={handleFilmChange}
                             type="text"
                             className="form-control"
                             aria-label="Sizing example input"
@@ -43,14 +93,21 @@ const FilmForm = () => {
 
                     <div className="input-group mb-3">
                         <input
+                            name="image"
                             type="file"
-                            className="form-control" />
-                        <label className="input-group-text" for="inputGroupFile02">Upload</label>
+                            className="form-control"
+                            onChange={handleFilmChange} />
+                        <label className="input-group-text" >Upload</label>
                     </div>
 
+                    {/* ANTEPRIMA IMMAGINE */}
+                    <img className="thumb mb-3" src={imagePreview} alt="Anteprima upload"></img>
                     <div className="input-group">
                         <span className="input-group-text">Abstract</span>
                         <textarea
+                            name="abstract"
+                            value={newFilm.abstract}
+                            onChange={handleFilmChange}
                             className="form-control"
                             aria-label="With textarea"></textarea>
                     </div>
